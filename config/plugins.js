f53ed4dc8eeaf17ada964ac42b745cd9bc7e2022
Anton Chernous <a.chernous@notamedia.ru>
2022-06-25 12:13:16 +0300

    add home page content

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,30 @@
-module.exports = () => {
+module.exports = ({ env }) => {
   return {
     ckeditor: "true",
     seo: {
       enabled: "true",
+    },
+    navigation: {
+      enabled: "true",
+      config: {
+        additionalFields: ["audience"],
+        contentTypes: ["api::page.page"],
+        contentTypesNameFields: {
+          "api::page.page": ["title"],
+        },
+        allowedLevels: 2,
+      },
+    },
+    slugify: {
+      enabled: "true",
+      config: {
+        contentTypes: {
+          post: {
+            field: "slug",
+            references: "title",
+          },
+        },
+      },
     },
   };
 };
